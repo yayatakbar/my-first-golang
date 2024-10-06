5d0f2c061081922a2579fb4fbfc1e4e42df89f0b
mhmdsakbar <yayatakbar@gmail.com>
2024-10-07 03:36:55 +0700

    elementary

--- main.go
+++ main.go
@@ -1,25 +1,21 @@
 package main
 
-import (
-	"fmt"
-)
-
-//TIP To run your code, right-click the code and select <b>Run</b>. Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.
+import "fmt"
 
 func main() {
-	//TIP Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined or highlighted text
-	// to see how GoLand suggests fixing it.
-	s := "gopher"
-	fmt.Println("Hello and welcome, %s!", s)
 
-	for i := 1; i <= 5; i++ {
-		//TIP You can try debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>. To start your debugging session,
-		// right-click your code in the editor and select the <b>Debug</b> option.
-		fmt.Println("i =", 100/i)
-	}
+	//Number
+	fmt.Println("satu =", 1)
+	fmt.Println("dua =", 2)
+	fmt.Println("tiga koma lima =", 3.5)
+
+	//Boolean
+	fmt.Println("True =", true)
+	fmt.Println("False =", false)
+
+	//String
+	fmt.Println("Hello World")
+	fmt.Println(len("Hello World"))
+	fmt.Println(("Hello World"[1]))
+
 }
-
-//TIP See GoLand help at <a href="https://www.jetbrains.com/help/go/">jetbrains.com/help/go/</a>.
-// Also, you can try interactive lessons for GoLand by selecting 'Help | Learn IDE Features' from the main menu.
